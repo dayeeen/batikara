7048bdb0858e7634097005df14881ea6223a3831
dayeeen <nubicodes@gmail.com>
2025-07-29 23:03:34 +0700

    add Hilt navigation compose dependency and update app theme to Material3

--- feature/auth/build.gradle.kts
+++ feature/auth/build.gradle.kts
@@ -48,6 +48,8 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
+    implementation(libs.androidx.hilt.navigation.compose)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     testImplementation(libs.junit)
