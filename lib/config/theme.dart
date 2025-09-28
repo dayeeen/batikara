d493dd04add96c8fd5d9aa1709b3a5029025eb9f
dayeeen <nubicodes@gmail.com>
2025-09-28 20:17:52 +0700

    feat: implement app theme and text styles with Google Fonts

---
+++ lib/config/theme.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppColors {
+  static const gold = Color(0xffEDBE6B);
+  static const brown = Color(0xff754219);
+  static const black = Color(0xff050505);
+  static const white = Color(0xffFFF9F9);
+  static const grey = Color(0xffA5A5A5);
+  static const darkGrey = Color(0xff2D2D2D);
+}
+
+class AppSpacing {
+  static const double edge = 24;
+}
+
+class AppText {
+  // Auth
+  static const String loginTitle = "Good to see you again";
+  static const String loginSubtitle = "Reconnect with your saved items, AI recommendations, and your favorite local brands.";
+  static const String registerTitle = "Create Your Batikara Account";
+  static const String registerSubtitle = "Join a movement to support local artisans and find batik pieces that match your unique style.";
+  
+  static const String login = "LOGIN";
+  static const String register = "REGISTER";
+
+  static const String orcw = "Or continue with";
+
+  static const String donthaveacc = "Don't have an account?";
+  static const String haveacc = "Already have an account?";
+
+  static const String signuphere = "Sign Up here";
+  static const String signinhere = "Sign In here";
+  
+
+  static const String signInToContinue = "Sign in to continue";
+  static const String email = "Email";
+  static const String password = "Password";
+  static const String confirmPassword = "Confirm Password";
+  static const String forgotPassword = "Forgot Password?";
+
+}
+
+class AppTextStyle {
+  static final title = GoogleFonts.plusJakartaSans(
+    fontWeight: FontWeight.w800,
+    color: AppColors.brown,
+    fontSize: 20,
+  );
+
+  static final subtitle = GoogleFonts.plusJakartaSans(
+    fontWeight: FontWeight.w500,
+    color: AppColors.black,
+    fontSize: 12,
+  );
+
+  static final label = GoogleFonts.plusJakartaSans(
+    fontWeight: FontWeight.w500,
+    color: AppColors.brown,
+    fontSize: 14,
+  );
+
+  static final actionText = GoogleFonts.plusJakartaSans(
+    fontWeight: FontWeight.w800,
+    color: AppColors.brown,
+    fontSize: 14,
+  );
+
+  static final black = GoogleFonts.plusJakartaSans(
+    fontWeight: FontWeight.bold,
+    color: AppColors.black,
+  );
+
+  static final white = GoogleFonts.plusJakartaSans(
+    fontWeight: FontWeight.w500,
+    color: AppColors.white,
+  );
+
+  static final grey = GoogleFonts.plusJakartaSans(
+    fontWeight: FontWeight.w300,
+    color: AppColors.grey,
+  );
+
+  static final gold = GoogleFonts.plusJakartaSans(
+    fontWeight: FontWeight.w500,
+    color: AppColors.gold,
+  );
+
+  static final regular = GoogleFonts.plusJakartaSans(
+    fontWeight: FontWeight.w400,
+    color: AppColors.black,
+  );
+}
+
+class AppTheme {
+  static ThemeData get lightTheme {
+    return ThemeData(
+      useMaterial3: true,
+      primaryColor: AppColors.gold,
+      scaffoldBackgroundColor: AppColors.white,
+      // AppTheme.lightTheme
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: AppColors.gold, // <— ganti ke gold
+        primary: AppColors.gold,
+        secondary: AppColors.brown,
+        background: AppColors.white,
+      ),
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        foregroundColor: AppColors.brown,
+      ),
+      textTheme: TextTheme(
+        bodyLarge: AppTextStyle.regular,
+        bodyMedium: AppTextStyle.grey,
+        titleLarge: AppTextStyle.black,
+        titleMedium: AppTextStyle.gold,
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppColors.gold,
+          foregroundColor: AppColors.black,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          textStyle: AppTextStyle.black,
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,48 @@
+import 'package:batikara/screens/auth/register_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'config/theme.dart';
+import 'screens/splash/splash_screen.dart';
+import 'screens/auth/login_screen.dart';
+import 'screens/home/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const BatikaraApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class BatikaraApp extends StatelessWidget {
+  const BatikaraApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(),
+    final router = GoRouter(
+      initialLocation: '/', // mulai dari splash
+      routes: [
+        GoRoute(
+          path: '/',
+          // builder: (context, state) => const SplashScreen(),
+          builder: (context, state) => const RegisterScreen(),
+        ),
+        GoRoute(
+          path: '/login',
+          builder: (context, state) => const LoginScreen(),
+        ),
+        GoRoute(
+          path: '/register',
+          builder: (context, state) => const RegisterScreen(),
+        ),
+        GoRoute(
+          path: '/home',
+          builder: (context, state) => const HomeScreen(),
+        ),
+      ],
     );
-  }
-}
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Text("Batikara", style: TextStyle(color: Colors.amber),),
-      ),
+    return MaterialApp.router(
+      title: 'Batikara',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      routerConfig: router,
     );
   }
 }
